81c79c6c037c3fe3136ce9f77198bee29e766b30
vadimdudko <v.dudko93@gmail.com>
2022-12-19 23:20:59 +0300

    feat: add system info

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
-import { homedir } from 'os';
 import { join } from 'path';
 import * as readline from 'node:readline/promises';
 import {getGoodbye, getWelcome} from "./helper.js";
 import {getUp} from "./up.js";
+import {getOEL} from "./os/eol.js";
+import {homedir} from 'node:os'
+import {getArch} from "./os/arch.js";
+import {getUsername} from "./os/username.js";
+import {getHomedir} from "./os/homedir.js";
+import {getCpus} from "./os/cpus.js";
 
-let username = '';
+let userName = '';
 
 const parseArgv = () => {
     const user = process.argv.reduce((acc, value) => {
         if (value.startsWith('--username=')) {
-            username = `${value.trim().replace('--username=','')}`;
-            return [...acc, getWelcome(username)];
+            userName = `${value.trim().replace('--username=','')}`;
+            return [...acc, getWelcome(userName)];
         }
         return acc;
     },[])
@@ -24,7 +29,7 @@ const rl = readline.createInterface({
 });
 
 process.on('SIGINT', () => {
-    getGoodbye(username);
+    getGoodbye(userName);
     process.exit();
 });
 
@@ -34,11 +39,26 @@ rl.on('line', (input) => {
     try {
         switch (input.trim()) {
             case '.exit':
-                getGoodbye(username);
+                getGoodbye(userName);
                 process.exit();
                 break;
             case 'up':
                 getUp();
+                break;
+            case 'os --EOL':
+                getOEL();
+                break;
+            case 'os --architecture':
+                getArch();
+                break;
+            case 'os --username':
+                getUsername();
+                break;
+            case 'os --homedir':
+                getHomedir();
+                break;
+            case 'os --cpus':
+                getCpus();
                 break;
             default:
                 console.log('Invalid input');
@@ -47,3 +67,6 @@ rl.on('line', (input) => {
         throw new Error('Operation failed')
     }
 });
+
+
+
